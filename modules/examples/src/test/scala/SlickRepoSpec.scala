8fbcc729782d94d2bf85722831f1c90c01803828
Cláudio Diniz <cfpdiniz@gmail.com>
2017-06-24 23:08:27 +0100

    Solving same case class boilerplate issue without macros

--- modules/examples/src/test/scala/SlickRepoSpec.scala
+++ modules/examples/src/test/scala/SlickRepoSpec.scala
@@ -25,4 +25,43 @@ class SlickRepoSpec extends WordSpec with Matchers {
     PersonVersioned(Some("2"),Some(1L)).withVersion(2L) shouldEqual PersonVersioned(Some("2"),Some(2L))
   }
 
+
+  //Solving same problem without macros
+
+  object CopyableEntity{
+    type Copyable = {
+      def copy[X,Z](param: Option[Z]) : X
+    }
+    implicit def toCopyable[T <: CopyableEntity[T, ID], ID]( base: CopyableEntity[T,ID] ): CopyableEntity[T,ID] with Copyable = base.asInstanceOf[CopyableEntity[T,ID] with Copyable]
+  }
+
+  trait CopyableEntity[T <: CopyableEntity[T, ID], ID] extends Entity[T,ID] {
+    def withId(id: ID): T = (this).copy[T,ID](Some(id))
+  }
+
+  case class PersonCopy(override val id : Option[String]) extends CopyableEntity[PersonCopy,String]
+
+  "`withId` should use case classs copy" in {
+    PersonCopy(Some("2")).withId("3") shouldEqual PersonCopy(Some("3"))
+  }
+
+  object CopyableVersionEntity{
+    type Copyable = {
+      def copy[X,Y,Z](id: Option[Y],param: Option[Z]) : X
+    }
+    implicit def toCopyable[T <: CopyableVersionEntity[T, ID, V], ID, V]( base: CopyableVersionEntity[T,ID,V] ): CopyableVersionEntity[T,ID,V] with Copyable = base.asInstanceOf[CopyableVersionEntity[T,ID,V] with Copyable]
+  }
+
+  trait CopyableVersionEntity[T <: CopyableVersionEntity[T, ID, V], ID, V] extends VersionedEntity[T,ID,V] {
+    def withId(id: ID): T = (this).copy[T,ID,V](Some(id),version)
+    def withVersion(version: V): T = (this).copy[T,ID,V](id,Some(version))
+  }
+
+
+  case class PersonCopyVersion(override val id : Option[String], override val version : Option[String]) extends CopyableVersionEntity[PersonCopyVersion,String,String]
+
+  "`withVersion` should use case classs copy" in {
+    PersonCopyVersion(Some("2"),Some("2")).withId("3").withVersion("3") shouldEqual PersonCopyVersion(Some("3"),Some("3"))
+  }
+
 }
